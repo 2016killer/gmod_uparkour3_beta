47f715a6374bcd69fe19156e5db66357419f6552
2016killer <ava741963@163.com>
2026-01-01 00:08:45 +0800

    整理一下格式

--- lua/vmanip/anims/dparkour.lua
+++ lua/vmanip/anims/dparkour.lua
@@ -7,36 +7,30 @@ local animname = function(str)
     return string.format('dp_%s_%s', str, author)
 end
 
-VManip:RegisterAnim(animname('catch'),
-    {
-        ["model"]=handModel,
-        ["lerp_peak"]=1,
-        ["lerp_speed_in"]=12,
-        ["lerp_speed_out"]=12,
-        ["lerp_curve"]=1,
-        ["speed"]=6
-    }
-)
+VManip:RegisterAnim(animname('catch'), {
+    ["model"]=handModel,
+    ["lerp_peak"]=1,
+    ["lerp_speed_in"]=12,
+    ["lerp_speed_out"]=12,
+    ["lerp_curve"]=1,
+    ["speed"]=6
+})
 
 
-VMLegs:RegisterAnim(animname('lazy'), 
-    {
-        ["model"]=legsModel,
-        ["speed"]=1.5,
-        ["forwardboost"]=2,
-        ["upwardboost"]=-5
-    }
-)
+VMLegs:RegisterAnim(animname('lazy'), {
+    ["model"]=legsModel,
+    ["speed"]=1.5,
+    ["forwardboost"]=2,
+    ["upwardboost"]=-5
+})
 
 
-VMLegs:RegisterAnim(animname('monkey'), 
-    {
-        ["model"]=legsModel,
-        ["speed"]=1.2,
-        ["forwardboost"]=-10,
-        ["upwardboost"]=-5
-    }
-)
+VMLegs:RegisterAnim(animname('monkey'), {
+    ["model"]=legsModel,
+    ["speed"]=1.2,
+    ["forwardboost"]=-10,
+    ["upwardboost"]=-5
+})
 
 VMLegs.RegisterAnimCollection = function(self, collectionName, anim)
     if not isstring(anim) then
